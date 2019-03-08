b816517c25426993fac7ee19578f0cbad1449347
JingGH <zhangjing@njfu.edu.cn>
2019-03-08 21:28:58 +0800

    'Day1'

---
+++ ex3.py
@@ -0,0 +1,29 @@
+# 我要算一下我有多少鸡
+print("I will now count my chickens:")
+
+#母鸡多少？公鸡多少？
+print("Hens",25 + 30 / 6)
+print("Roosters",100 - 25 * 3 % 4)
+
+#现在数一数有多少蛋
+print("Now I will count the eggs:")
+
+#这个对吗？
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+#算一算
+print("What is 3 + 2?",3 + 2)
+print("What is 5 - 7?",5 - 7)
+
+#哈哈哈
+print("Oh,that's why it's Flase.")
+
+#再来一点怎么样？
+print("How about some more.")
+
+#对吗？
+print("Is it greater?",5 > -2)
+print("Is it greater or equal?",5 >= -2)
+print("Is it less or equal?",5 <= -2)
